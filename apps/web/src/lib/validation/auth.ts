be9b24dab00a22505f6f88097c46947cfccb66b2
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2026-02-11 09:56:52 +0200

    feat(web): add sign up form

--- apps/web/src/lib/validation/auth.ts
+++ apps/web/src/lib/validation/auth.ts
@@ -1,7 +1,20 @@
 import { z } from "zod";
 
 export const signInSchema = z.object({
-	email: z.email("The provided email is invalid"),
+	email: z.email("Provided email is invalid"),
 	password: z.string("Password is required"),
 });
 export type SignIn = z.infer<typeof signInSchema>;
+
+export const signUpSchema = z
+	.object({
+		name: z.string().min(2, "Name must have at least 2 characters").optional(),
+		email: z.email("Provided email is invalid"),
+		password: z.string().min(8),
+		confirmPassword: z.string(),
+	})
+	.refine((data) => data.password === data.confirmPassword, {
+		message: "Passwords do not match",
+		path: ["confirmPassword"],
+	});
+export type SignUp = z.infer<typeof signUpSchema>;
