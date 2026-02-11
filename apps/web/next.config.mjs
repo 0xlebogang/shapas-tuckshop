551cdf60157b92eaaffb08bdb5222cb26de69011
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2026-02-11 16:04:20 +0200

    feat(web): add api request proxying

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -2,6 +2,16 @@
 const nextConfig = {
 	reactCompiler: true,
 	transpilePackages: ["@repo/better-auth"],
+	rewrites: async () => {
+		return {
+			beforeFiles: [
+				{
+					source: "/api/:path*",
+					destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
+				},
+			],
+		};
+	},
 };
 
 export default nextConfig;
