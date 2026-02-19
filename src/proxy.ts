9d6f896a7d0c8a1c99e1bbead8385c7d35dd5e28
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2026-02-19 12:42:51 +0200

    feat: add auth based redirection

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,6 +1,20 @@
-import { convexAuthNextjsMiddleware } from "@convex-dev/auth/nextjs/server";
+import {
+	convexAuthNextjsMiddleware,
+	createRouteMatcher,
+	nextjsMiddlewareRedirect as redirect,
+} from "@convex-dev/auth/nextjs/server";
 
-export default convexAuthNextjsMiddleware();
+const isSignInPage = createRouteMatcher(["/signin"]);
+const isProtectedRoute = createRouteMatcher(["/", "/product(.*)"]);
+
+export default convexAuthNextjsMiddleware(async (request, { convexAuth }) => {
+	if (isSignInPage(request) && (await convexAuth.isAuthenticated)) {
+		return redirect(request, "/");
+	}
+	if (isProtectedRoute(request) && !(await convexAuth.isAuthenticated())) {
+		return redirect(request, "/auth");
+	}
+});
 
 export const config = {
 	matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
