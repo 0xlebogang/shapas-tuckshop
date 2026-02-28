5dd3390af9db40a6e6ac1f50978894e5af1fa327
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2026-02-28 09:29:33 +0200

    feat(backend): add server initializer

--- packages/backend/cmd/main/main.go
+++ packages/backend/cmd/main/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/0xlebogang/shapas/backend/internal/config"
+	"github.com/0xlebogang/shapas/backend/internal/database"
+	"github.com/0xlebogang/shapas/backend/internal/server"
+)
 
 func main() {
-	fmt.Println("Hello from backend")
+	config := config.LoadEnv()
+
+	db, err := database.Connect(config.DatabaseUrl)
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+	defer database.Close(db)
+
+	server := server.New(config, db)
+	if err := server.Start(); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
 }
